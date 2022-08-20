dfec2248a9c4124ec029d7b711eba9ce723b272f
Aleksana <me@aleksana.moe>
2022-08-20 20:01:49 +0800

    introduce run_command in favor of os.popen

---
+++ src/tools/command.py
@@ -0,0 +1,30 @@
+from sys import stdout
+import subprocess
+
+
+class run_command():
+
+    def __init__(self,command: str,error=False):
+        encoding = stdout.encoding
+        try:
+            result_object = subprocess.run(
+                command.split(),
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+            if (result_object.returncode == 0) is not error:
+                self.result = result_object.stdout.decode(encoding)
+            else:
+                self.result = ""
+        except (FileNotFoundError, AttributeError):
+            self.result = ""
+
+    def read(self):
+        return self.result
+
+    def readline(self):
+        return self.result.split("\n", 1)[0]
+
+    def readlines(self):
+        return self.result.split("\n")
