f09bcff97fd25e4c2b4a9f51854f91f637cf21f2
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-08-20 10:11:23 -0400

    [F] Fix memory display

--- src/sysinfo/memory/__init__.py
+++ src/sysinfo/memory/__init__.py
@@ -10,8 +10,6 @@ def get(result):
     else:
         pass
 
-    memory_used_mib = str(int(memory_used / (1024^2)))
-    memory_all_mib = str(int(memory_all / (1024^2)))
+    memory = f"{memory_used / 1024 ** 2:.1f} GiB / {memory_all / 1024 ** 2:.1f} GiB"
 
-    memory = "%sMiB / %sMiB" % (memory_used_mib, memory_all_mib)
     result['Memory'] = memory
