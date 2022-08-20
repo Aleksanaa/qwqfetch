55ba13fe9a6ac533eefe171b8022872cb8d69076
Aleksana <me@aleksana.moe>
2022-08-20 21:50:04 +0800

    qwq

--- src/__init__.py
+++ src/__init__.py
@@ -1,26 +1,27 @@
 from . import globals
-
-globals._init()
-
 from .basic_system_info import *
 from .default_result_list import default_result
 
-globals.set(
-    {
-        "platform": {"name": sys_name, "type": sys_type, "arch": sys_arch},
-        "result": {result: "" for result in default_result},
-        "args": {},
-    }
-)
+def get_result_dict():
+    globals._init()
 
-from . import sysinfo
+    globals.set(
+        {
+            "platform": {"name": sys_name, "type": sys_type, "arch": sys_arch},
+            "result": {result: "" for result in default_result},
+            "args": {},
+        }
+    )
 
-sysinfo.run()
+    from . import sysinfo
 
-result_dict = globals.get(["result"])[0]
+    sysinfo.run()
+
+    return globals.get(["result"])[0]
 
 
 def get_result():
+    result_dict = get_result_dict()
     result = "{}@{}\n".format(result_dict.pop("USERNAME"), result_dict.pop("HOSTNAME"))
     result += "-" * (len(result) - 1) + "\n"
     for key in default_result:
