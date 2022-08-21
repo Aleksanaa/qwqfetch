8ba144068485a527c8105131329cc478f9c7adff
Aleksana <me@aleksana.moe>
2022-08-21 11:17:39 +0800

    introduce parse_info

---
+++ src/tools/parse_info.py
@@ -0,0 +1,27 @@
+# It is used to deal with this kind of strings:
+
+# Name: Arch Linux
+# Arch: x86_64
+# Version:  Rolling
+
+# or
+
+# Name=Arch Linux
+# Arch=x86_64
+# Version=Rolling
+
+# And if you need Name and Arch:
+# needs = {"Name":"name","Arch":"arch"}
+# It will output {"name":"Arch Linux","arch":"x86_64"}
+
+
+def parser(inputs: str, needs: dict, separator: str):
+    satisfied = {}
+    for line in inputs.split("\n"):
+        for key, val in needs.items():
+            if val not in satisfied.values():
+                if line.strip().startswith(key):
+                    line_list = line.split(separator, 1)
+                    if line_list[0].strip() == key:
+                        satisfied[val] = line_list[1]
+    return satisfied
