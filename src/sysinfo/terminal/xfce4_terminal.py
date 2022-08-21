5e4a18f9961f52620f56939cc589172eb1428556
Aleksana <me@aleksana.moe>
2022-08-21 13:29:33 +0800

    various fix

--- src/sysinfo/terminal/xfce4_terminal.py
+++ src/sysinfo/terminal/xfce4_terminal.py
@@ -5,17 +5,20 @@ from ...tools import parse_info
 def get_font():
     xfce4_config = f"{expanduser('~')}/.config/xfce4/terminal/terminalrc"
     needs = {"FontName": "font_name", "FontUseSystem": "use_system"}
-    results = parse_info.parser(open(xfce4_config).read(), needs, "=")
-    if "use_system" in results and results["use_system"] == "TRUE":
-        from ...tools.command import run_command
+    try:
+        results = parse_info.parser(open(xfce4_config).read(), needs, "=")
+        if "use_system" in results and results["use_system"] == "TRUE":
+            from ...tools.command import run_command
 
-        font_name = (
-            run_command("gsettings get org.gnome.desktop.interface monospace-font-name")
-            .read()
-            .strip("'")
-        )
-        if font_name != "":
-            return font_name
+            font_name = (
+                run_command("gsettings get org.gnome.desktop.interface monospace-font-name")
+                .read()
+                .strip("'")
+            )
+            if font_name != "":
+                return font_name
 
-    elif "font_name" in results:
-        return results["font_name"]
+        elif "font_name" in results:
+            return results["font_name"]
+    except FileNotFoundError:
+        pass
