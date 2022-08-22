277e58f20c7880cfbef6330ed4ad24dccf11bf30
Aleksana <me@aleksana.moe>
2022-08-22 21:05:18 +0800

    also support newer and preview version

--- src/sysinfo/shell/windows.py
+++ src/sysinfo/shell/windows.py
@@ -13,12 +13,13 @@ if shell_name == "cmd":
         RunCommand("cmd -ver").readline().split("[")[1].split()[-1].strip("]").strip()
     )
 
-elif shell_name == "powershell":
+elif shell_name == "powershell" or "pwsh":
+    command = "pwsh -Command" if shell_name == "pwsh" else "powershell"
     shell_ver = ".".join(
         [
             s
             for s in (
-                RunCommand("powershell $PSVersionTable.PSVersion")
+                RunCommand(f"{command} $PSVersionTable.PSVersion")
                 .read()
                 .split("\n")[2]
                 .split()
@@ -26,6 +27,7 @@ elif shell_name == "powershell":
             if s
         ]
     )
+    shell_ver = shell_ver.replace(".preview", "-preview")
 
 elif shell_name == "bash":
     bash_path = "C:\\Program Files\\Git\\bin\\bash.exe"
